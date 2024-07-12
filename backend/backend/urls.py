b698cf4e616163bc26617dcb32e26f08112fbb0c
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-07-12 12:22:45 -0400

    urls fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipe_api.urls'))
+    path('', include('recipes_api.urls'))
 ]
--- backend/recipes_api/urls.py
+++ backend/recipes_api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/recipes', views.RecipeList.as_view(), name='recipe_list')
+    path('api/recipes', views.RecipeList.as_view(), name='recipe_list'),
     path('api/recipes/<int:pk>', views.RecipeDetail.as_view(), name='recipe_detail')
 ]
