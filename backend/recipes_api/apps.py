c89aaed7429ac1be31129196b4f8c5ed71ee0fc1
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-07-12 17:19:34 -0400

    removed unnecessary signal file

--- backend/recipes_api/apps.py
+++ backend/recipes_api/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class RecipesApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recipes_api'
-
-    # def ready(self):
-    #     import recipes_api.signals
--- backend/recipes_api/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import Recipe, Ingredient_List, Instruction_List
-
-@receiver(post_save, sender=Recipe)
-def create_lists(sender, instance, created, **kwargs):
-    if created:
-        Ingredient_List.objects.create(recipe=instance)
-        Instruction_List.objects.create(recipe=instance)
