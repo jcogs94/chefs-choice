e57698f91ec091204a329dba983f1ec633250948
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-07-12 09:49:25 -0400

    superuser created, models registered to admin

--- backend/recipes_api/admin.py
+++ backend/recipes_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Recipe, Instruction, Ingredient_List, Ingredient
 
-# Register your models here.
+admin.site.register(Recipe)
+admin.site.register(Instruction)
+admin.site.register(Ingredient_List)
+admin.site.register(Ingredient)
