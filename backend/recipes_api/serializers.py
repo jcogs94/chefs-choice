7e257a96cadc049872af40f84056089168f1b564
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-07-15 11:47:56 -0400

    serializers corrected for proper req res of api

--- backend/recipes_api/serializers.py
+++ backend/recipes_api/serializers.py
@@ -4,11 +4,11 @@ from .models import Recipe, Ingredient, Ingredient_List, Instruction, Instructio
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
-        fields = ['amount', 'measurement', 'name']
+        fields = ['name']
 
 class IngredientListSerializer(serializers.ModelSerializer):
-    ingredients = IngredientSerializer(many=True)
-    
+    ingredients = serializers.ListField(child=serializers.CharField())
+
     class Meta:
         model = Ingredient_List
         fields = ['ingredients']
@@ -16,6 +16,17 @@ class IngredientListSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         ingredients = instance.ingredients.all()
         return [ingredient.name for ingredient in ingredients]
+    
+    def to_internal_value(self, data):
+        if isinstance(data, list):
+            return {'ingredients': data}
+        ingredients = data.get('ingredients', [])
+        if not isinstance(ingredients, list):
+            raise serializers.ValidationError({
+                'ingredients': 'Expected a list of strings.'
+            })
+        return {'ingredients': ingredients}
+
 
 class InstructionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,7 +34,7 @@ class InstructionSerializer(serializers.ModelSerializer):
         fields = ['text']
 
 class InstructionListSerializer(serializers.ModelSerializer):
-    instructions = InstructionSerializer(many=True)
+    instructions = serializers.ListField(child=serializers.CharField())
 
     class Meta:
         model = Instruction_List
@@ -32,6 +43,18 @@ class InstructionListSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         instructions = instance.instructions.all()
         return [instruction.text for instruction in instructions]
+    
+    def to_internal_value(self, data):
+        if isinstance(data, list):
+            return {'instructions': data}
+        instructions = data.get('instructions', [])
+        if not isinstance(instructions, list):
+            raise serializers.ValidationError({
+                'instructions': 'Expected a list of strings.'
+            })
+        return {'instructions': instructions}
+
+
 
 class RecipeSerializer(serializers.ModelSerializer):
     ingredients = IngredientListSerializer(source='ingredient_list', required=False)
@@ -42,13 +65,45 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'style', 'time_to_complete', 'description', 'img_ref', 'ingredients', 'instructions']
     
     def create(self, validated_data):
-        recipe = Recipe.objects.create(
-            name=validated_data['name'],
-            style=validated_data['style'],
-            time_to_complete=validated_data['time_to_complete'],
-            description=validated_data['description'],
-            img_ref=validated_data['img_ref']
-        )
-        Ingredient_List.objects.create(recipe=recipe)
-        Instruction_List.objects.create(recipe=recipe)
+        ingredients_data = validated_data.pop('ingredient_list', {}).get('ingredients', [])
+        instructions_data = validated_data.pop('instruction_list', {}).get('instructions', [])
+        
+        recipe = Recipe.objects.create(**validated_data)
+        ingredient_list = Ingredient_List.objects.create(recipe=recipe)
+        instruction_list = Instruction_List.objects.create(recipe=recipe)
+
+        for ingredient_name in ingredients_data:
+            ingredient = Ingredient.objects.create(name=ingredient_name)
+            ingredient_list.ingredients.add(ingredient)
+
+        for instruction_text in instructions_data:
+            Instruction.objects.create(list=instruction_list, text=instruction_text)
+        
         return recipe
+
+    def update(self, instance, validated_data):
+        ingredients_data = validated_data.pop('ingredient_list', {}).get('ingredients', [])
+        instructions_data = validated_data.pop('instruction_list', {}).get('instructions', [])
+        
+        instance.name = validated_data.get('name', instance.name)
+        instance.style = validated_data.get('style', instance.style)
+        instance.time_to_complete = validated_data.get('time_to_complete', instance.time_to_complete)
+        instance.description = validated_data.get('description', instance.description)
+        instance.img_ref = validated_data.get('img_ref', instance.img_ref)
+        instance.save()
+
+        if ingredients_data:
+            ingredient_list = instance.ingredient_list
+            ingredient_list.ingredients.clear()
+            for ingredient_name in ingredients_data:
+                ingredient = Ingredient.objects.create(name=ingredient_name)
+                ingredient_list.ingredients.add(ingredient)
+        
+        if instructions_data:
+            instruction_list = instance.instruction_list
+            instruction_list.instructions.all().delete()
+            for instruction_text in instructions_data:
+                Instruction.objects.create(list=instruction_list, text=instruction_text)
+
+        return instance
+
