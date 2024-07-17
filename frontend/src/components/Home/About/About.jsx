70dc4bb2a90383eee495a52cc29924c0e17e3372
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-07-17 17:08:18 -0400

    added text to about section

--- frontend/src/components/Home/About/About.jsx
+++ frontend/src/components/Home/About/About.jsx
@@ -3,8 +3,20 @@ import './About.css'
 const About = () => {
     return <>
         <div id="about">
-            <h3>About Chefs Choice</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi nihil reiciendis, repudiandae aut nam quo harum, inventore fugiat hic facilis necessitatibus, officiis assumenda qui delectus deserunt veniam laudantium accusamus maiores.</p>
+            <h3>About Chef's Choice</h3>
+            <div className="about-par">
+                <h4>Discover New Recipes</h4>
+                <p>Dive into our extensive collection of recipes curated from chefs and food enthusiasts around the globe. From mouth-watering appetizers to decadent desserts, our diverse selection ensures there’s something to satisfy every palate and occasion.</p>
+            </div>
+            <div className="about-par">
+                <h4>Share Your Creations</h4>
+                <p>Have a recipe that you love and want to share with the world? Chef's Choice makes it easy to contribute your culinary masterpieces. Simply upload your recipe, complete with ingredients, instructions, and photos, and become part of our vibrant community of food lovers.</p>
+            </div>
+            <div className="about-par">
+                <h4>Community and Connection</h4>
+                <p>At Chef's Choice, we believe in the power of food to bring people together. Connect with other food enthusiasts, exchange tips and techniques, and celebrate the joy of cooking and sharing meals with others. Our platform is designed to foster a supportive and engaging environment where everyone can learn and grow.</p>
+            </div>
+            <p>Join us at Chef's Choice and embark on a delicious adventure. Whether you’re looking to discover new recipes or share your own, we’re excited to have you as part of our community. Let’s cook, share, and savor the flavors together!</p>
         </div>
     </>
 }
