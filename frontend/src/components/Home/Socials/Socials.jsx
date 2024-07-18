2252cb3b7f66d5fb01822c64738730a8656ab8ca
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-07-18 07:04:40 -0400

    socials now placeholder links

--- frontend/src/components/Home/Socials/Socials.jsx
+++ frontend/src/components/Home/Socials/Socials.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import facebook from './icons/facebook.png'
 import instagram from './icons/instagram.png'
 import twitter from './icons/twitter.png'
@@ -5,14 +6,19 @@ import youtube from './icons/youtube.png'
 import './Socials.css'
 
 const Socials = () => {
-    const icons = [facebook, instagram, twitter, youtube]
+    const icons = [
+        { img: facebook, link: '/facebook'},
+        { img: instagram, link: '/instagram'},
+        { img: twitter, link: '/twitter'},
+        { img: youtube, link: '/youtube'}
+    ]
     
     return <>
         <div id="socials">
             <h3>Check Out Our Socials!</h3>
             <div id="social-icons">
                 {icons.map( (icon, index) => 
-                    <img src={icon} key={`icon-${index}`} />
+                    <Link to={icon.link}><img src={icon.img} key={`icon-${index}`} /></Link>
                 )}
             </div>
         </div>
