3ab85e3121723e23bbf2c5c2e0bf9144d412eb23
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-07-18 19:08:53 -0400

    footer adjusted with links

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,19 @@
+import { Link } from 'react-router-dom'
 import './Footer.css'
 
 const Footer = () => {
     return <>
         <footer>
-            <h4>test</h4>
+            <div>
+                <Link to="/">Home</Link>
+                <Link to="/recipes">View Recipes</Link>
+                <Link to="/recipes/new">Add Your Own Recipe</Link>
+            </div>
+            <div>
+                <Link to="/privacy-policy">Privacy Policy</Link>
+                <Link to="/terms">Terms of Service</Link>
+                <Link to="/contact-us">Contact Us</Link>
+            </div>
         </footer>
     </>
 }
