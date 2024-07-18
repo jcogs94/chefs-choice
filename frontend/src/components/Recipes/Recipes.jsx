1e791cbb1184c45a2da4bae39b2f77590363c6e5
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-07-18 18:58:30 -0400

    favicon added

--- frontend/src/components/Recipes/Recipes.jsx
+++ frontend/src/components/Recipes/Recipes.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import * as recipeService from '../../services/recipeService.js'
 import RecipeCard from './RecipeCard/RecipeCard.jsx'
 import './Recipes.css'
@@ -26,6 +27,10 @@ const Recipes = () => {
     
     return <>
         <h1 id='recipes-header'>Recipes</h1>
+        <div id="add-recipe-here-container">
+          <p>Add your own recipe here:</p>
+          <Link to="/recipes/new"><button>Add Recipe</button></Link>
+        </div>
         <div id="recipes-container">
             {recipes.map( (recipe, index) => 
                 <RecipeCard recipe={recipe} key={index} />
